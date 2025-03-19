8e822b9cc353fc27e92178873f8e5527bb53ecc1
Kexin Chen <22360275@zju.edu.cn>
2025-03-20 00:03:58 +0800

    upload

---
+++ scripts/detect.sh
@@ -0,0 +1,40 @@
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 0 --model sentence-transformers/sentence-t5-base > sentence-t5-base.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 1 --model sentence-transformers/sentence-t5-large > sentence-t5-large.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 1 --model sentence-transformers/sentence-t5-xl > sentence-t5-xl.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 1 --model sentence-transformers/all-mpnet-base-v2 > all-mpnet-base-v2.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 2 --model sentence-transformers/all-MiniLM-L6-v2 > all-MiniLM-L6-v2.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 2 --model BAAI/bge-base-en-v1.5 > bge-base-en-v1.5.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 2 --model BAAI/bge-small-en-v1.5 > bge-small-en-v1.5.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 3 --model BAAI/bge-large-en-v1.5 > bge-large-en-v1.5.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 3 --model sentence-transformers/sentence-t5-xxl > sentence-t5-xxl.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 2 --model intfloat/e5-small > e5-small.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 2 --model intfloat/e5-base > e5-base.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 4 --model intfloat/e5-large > e5-large.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 4 --model intfloat/e5-large > e5-large.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 2 --model Alibaba-NLP/gte-Qwen2-1.5B-instruct > gte-Qwen2-1.5B-instruct.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 5 --model Salesforce/SFR-Embedding-2_R > SFR-Embedding-2_R.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 1 --model GritLM/GritLM-7B > GritLM-7B.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 4 --model sentence-transformers/gtr-t5-base > gtr-t5-base.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 4 --model sentence-transformers/gtr-t5-large > gtr-t5-large.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 2 --model sentence-transformers/gtr-t5-xl > gtr-t5-xl.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 2 --model thenlper/gte-small > gte-small.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 5 --model thenlper/gte-base > gte-base.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 5 --model Alibaba-NLP/gte-base-en-v1.5 > gte-base-en-v1.5.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 7 --model Alibaba-NLP/gte-Qwen2-7B-instruct --flash_attn True > gte-Qwen2-7B-instruct.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 2 --model Alibaba-NLP/gte-large-en-v1.5 > gte-large-en-v1.5.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 4 --model thenlper/gte-large > gte-large.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 4 --model intfloat/e5-mistral-7b-instruct > e5-mistral-7b-instruct.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 3 --model sentence-transformers/gtr-t5-xxl > gtr-t5-xxl.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 5 --model princeton-nlp/sup-simcse-bert-base-uncased > sup-simcse-bert-base-uncased.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 5 --model princeton-nlp/sup-simcse-bert-large-uncased > sup-simcse-bert-large-uncased.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 1 --model princeton-nlp/sup-simcse-roberta-base > sup-simcse-roberta-base.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 1 --model princeton-nlp/sup-simcse-roberta-large > sup-simcse-roberta-large.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 3 --model Salesforce/SFR-Embedding-2_R --flash_attn True > SFR-Embedding-2_R.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 3 --model Salesforce/SFR-Embedding-Mistral --flash_attn True > SFR-Embedding-Mistral.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 5 --model nomic-ai/nomic-embed-text-v1 > nomic-embed-text-v1.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 5 --model nomic-ai/nomic-embed-text-v1.5 > nomic-embed-text-v1.5.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 1 --model hkunlp/instructor-base > instructor-base.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 2 --model hkunlp/instructor-large --flash_attn True > instructor-large.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 2 --model hkunlp/instructor-xl --flash_attn True > instructor-xl.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect.py --gpu 4 --model WhereIsAI/UAE-Large-V1 > UAE-Large-V1.log 2>&1 &
+nohup G:/juchiyun2024-11-14/anaconda3/envs/magikarp/bin/python /root/StickyToken/pyscripts/detect_reload.py --gpu 2 --model sentence-transformers/sentence-t5-base > sentence-t5-base.log 2>&1 &
---
+++ scripts/detect_reload.py
@@ -0,0 +1,75 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some parameters.')
+parser.add_argument('--model', type=str, default='sentence-transformers/sentence-t5-base', help='Path to the model')
+parser.add_argument('--dataset', nargs='+', default=[
+    "mteb/sts13-sts",
+    "mteb/sts22-crosslingual-sts",
+    "mteb/sts12-sts",
+    "mteb/stsbenchmark-sts",
+    "mteb/sickr-sts",
+    "mteb/sts14-sts",
+    "mteb/biosses-sts",
+    "mteb/sts16-sts",
+    "mteb/sts15-sts",
+    "mteb/stsb_multi_mt",
+    "mteb/sts17-crosslingual-sts"
+], help='List of dataset paths')
+parser.add_argument('--sent_pair_num', type=int, default=5, help='Number of sentence pairs')
+parser.add_argument('--insert_num', type=int, default=8, help='Number of insertions')
+parser.add_argument('--verification_sent_pair_num', type=int, default=250, help='Number of verification sentence pairs')
+parser.add_argument('--gpu', type=int, default=0, help='GPU to use')
+parser.add_argument('--flash_attn', type=bool, default=False, help='Whether to use flash attention')
+args = parser.parse_args()
+
+class EXP:
+    MODEL=args.model
+    DATASET=args.dataset
+    SENT_PAIR_NUM=args.sent_pair_num
+    INSERT_NUM=args.insert_num
+    VERIFICATION_SENT_PAIR_NUM=args.verification_sent_pair_num
+
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu)
+import sys
+sys.path.append('/root/StickyToken')
+
+from stickytoken.embedding_model import ModelAnalyzer
+moda = ModelAnalyzer(EXP.MODEL,use_flash_attn=args.flash_attn)
+
+from stickytoken.tokenization import TokenizerAnalyzer
+toka = TokenizerAnalyzer(EXP.MODEL)
+
+token_infos = toka.categorize_tokens()
+
+# moda.check_on_unit_sphere()
+# moda.check_is_anisotropic()
+
+from stickytoken.sentence_pair import SentencePair,Dataset
+sent_pairs = SentencePair(EXP.DATASET,moda.model,moda.model_name,get_sentence_pairs_num=1000)
+dataset = Dataset(sent_pairs.sampled_sentence_pairs_path, moda, EXP.SENT_PAIR_NUM)
+verification_dataset = Dataset(sent_pairs.sampled_sentence_pairs_path,moda,EXP.VERIFICATION_SENT_PAIR_NUM)
+
+# token_infos_with_metrics = moda.caculate_vocab_token_magic_score(token_infos,
+#                                                                  dataset,
+#                                                                  EXP.INSERT_NUM,
+#                                                                  )
+
+# token_infos_with_metrics = moda.caculate_vocab_token_magic_score_multi_token(token_infos,
+#                                                                                 dataset,
+#                                                                                 EXP.INSERT_NUM,
+#                                                                                 )
+
+from stickytoken.utils import load_vocab_token_magic_scores,load_vocab_verifications
+# try:
+#     token_infos_with_metrics = load_vocab_verifications(moda.model_name)
+# except:
+token_infos_with_metrics = load_vocab_token_magic_scores(moda.model_name)
+
+moda.final_verification(token_infos_with_metrics,
+                        verification_dataset,
+                        EXP.INSERT_NUM,
+                        threshold_ratio=100,
+                        batch_size=256)
+
+moda.record_all(EXP)
